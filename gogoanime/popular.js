effe90df1d3fa1d134e35a41a0f2a354bf0e1269
MrCain <50195844+mrcainv1-3128@users.noreply.github.com>
2024-04-03 01:13:14 +0800

    Update popular.js

--- gogoanime/popular.js
+++ gogoanime/popular.js
@@ -1,4 +1,5 @@
-const axios = require('axios');
+//const axios = require('axios');
+import axios, { AxiosRequestConfig, AxiosPromise, AxiosResponse } from 'axios';
 const cheerio = require('cheerio');
 const fs = require('fs');
 
