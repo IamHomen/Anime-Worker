eec6e4edb2f92978011fae7decae8fba451da6bd
MrCain <50195844+mrcainv1-3128@users.noreply.github.com>
2024-04-04 19:43:43 +0800

    Create generate.py

---
+++ animepahe/generate.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+BASE_URL = 'https://animepahe.ru/api?m=airing&page=1'
+
+def scrape_recent_sub_anime():
+    
+    try:
+        headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36'}
+        anime_list = requests.get(BASE_URL, headers=headers)
+
+        with open('./animepahe/recent-sub.json', 'w') as f:
+            json.dump(anime_list, f, indent=2)
+        print('Data saved to animepahe/recent-sub.json')
+
+        return anime_list
+    except Exception as e:
+        print(e)
+        return {'error': str(e)}
+
+scrape_recent_sub_anime()
