c66ee1e07f643b1163acbbcd713c89acbb2b3b68
MrCain <50195844+mrcainv1-3128@users.noreply.github.com>
2024-04-04 21:01:53 +0800

    Update generate.py

--- anilist/generate.py
+++ anilist/generate.py
@@ -33,7 +33,7 @@ query {
 }
 '''
 
-def scrape_anilist_data():
+def scrape_anilist_poular():
     anime_list = []
     try:
         # Define the request headers
@@ -73,5 +73,77 @@ def scrape_anilist_data():
         print(e)
         return {'error': str(e)}
 
+# Define your GraphQL query
+trending = '''
+query {
+  Page(page: 1, perPage: 20) {
+    pageInfo {
+      total
+      perPage
+      currentPage
+      lastPage
+      hasNextPage
+    }
+    media(type: ANIME, sort: TRENDING_DESC) {
+      id
+      seasonYear
+      episodes
+      format
+      coverImage {
+        extraLarge
+      }
+      title {
+        english
+        userPreferred
+        romaji
+      }
+    }
+  }
+}
+'''
+
+def scrape_anilist_trending():
+    anime_list = []
+    try:
+        # Define the request headers
+        headers = {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+        
+        # Define the request body
+        data = {'query': trending}
+
+        # Make the request to the AniList GraphQL API
+        response = requests.post(GRAPHQL_URL, headers=headers, json=data)
+        response.raise_for_status()
+
+        # Parse the JSON response
+        result = response.json()
+
+        # Extract the anime data from the response
+        for anime in result['data']['Page']['media']:
+            anime_list.append({
+                'id': anime['id'],
+                'title': anime['title']['userPreferred'],
+                'cover': anime['coverImage']['extraLarge'],
+                'seasonYear': anime['seasonYear'],
+                'totalEpisode': anime['episodes'],
+                'format': anime['format']
+            })
+
+        # Save the data to a JSON file
+        with open('./anilist/trending.json', 'w') as f:
+            json.dump(anime_list, f, indent=2)
+        print('Data saved to anilist/trending.json')
+
+        return anime_list
+    except Exception as e:
+        print(e)
+        return {'error': str(e)}
+
 # Call the function to scrape AniList data
-scrape_anilist_data()
+scrape_anilist_trending()
+
+# Call the function to scrape AniList data
+scrape_anilist_popular()
