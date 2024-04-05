9b74f20eabb1c3194c133c8c89a41adbcf471750
MrCain <50195844+mrcainv1-3128@users.noreply.github.com>
2024-04-05 10:33:23 +0800

    Update generate.py

--- mangadex/generate.py
+++ mangadex/generate.py
@@ -15,29 +15,30 @@ def get_latest_manga_updates():
         result = response.json()
 
         # Extract the manga data from the response
-        for manga in result['data']:
-            for relationship in manga['relationships']:
-                if relationship['type'] == 'cover_art':
-                    cover_art_attributes = relationship['attributes']
-                    cover_art_file_name = cover_art_attributes['fileName']
-                    break  # Exit the loop once the correct relationship is found
+for manga in result['data']:
+    for relationship in manga['relationships']:
+        if relationship['type'] == 'cover_art':
+            cover_art_attributes = relationship['attributes']
+            cover_art_file_name = cover_art_attributes['fileName']
+            break  # Exit the loop once the correct relationship is found
 
-         mangaId = manga['id']
-         coverUrl = f"https://mangadex.org/covers/{mangaId}/{cover_art_file_name}"
-            manga_list.append({
-                'mangaId': mangaId,
-                'title': manga['attributes']['title']['en'],
-                'updatedAt': manga['attributes']['updatedAt'],
-                'coverArt': coverUrl,
-                'availableChapters': manga['attributes']['chapterCount'] if 'chapterCount' in manga['attributes'] else None,
-                'altTitles': manga['attributes']['altTitles'],
-                'description': manga['attributes']['description'],
-                'status': manga['attributes']['status'],
-                'releaseDate': manga['attributes']['year'],
-                'contentRating': manga['attributes']['contentRating'],
-                'lastVolume': manga['attributes']['lastVolume'],
-                'lastChapter': manga['attributes']['lastChapter']
-            })
+    mangaId = manga['id']
+    coverUrl = f"https://mangadex.org/covers/{mangaId}/{cover_art_file_name}"
+    manga_list.append({
+        'mangaId': mangaId,
+        'title': manga['attributes']['title']['en'],
+        'updatedAt': manga['attributes']['updatedAt'],
+        'coverArt': coverUrl,
+        'availableChapters': manga['attributes']['chapterCount'] if 'chapterCount' in manga['attributes'] else None,
+        'altTitles': manga['attributes']['altTitles'],
+        'description': manga['attributes']['description'],
+        'status': manga['attributes']['status'],
+        'releaseDate': manga['attributes']['year'],
+        'contentRating': manga['attributes']['contentRating'],
+        'lastVolume': manga['attributes']['lastVolume'],
+        'lastChapter': manga['attributes']['lastChapter']
+    })
+
 
         # Save the data to a JSON file
         with open('./mangadex/latest_manga_updates.json', 'w') as f:
