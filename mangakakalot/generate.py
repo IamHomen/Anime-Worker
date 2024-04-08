8356622febb0e2d82df40329cf45146b54c92ca9
MrCain <50195844+mrcainv1-3128@users.noreply.github.com>
2024-04-08 12:11:18 +0800

    Update generate.py

--- mangakakalot/generate.py
+++ mangakakalot/generate.py
@@ -4,6 +4,7 @@ import json
 
 LATEST_MANGA_URL = 'https://mangakakalot.com/manga_list?type=latest&category=all&state=all&page=1'
 BASE_URL = 'https://mangakakalot.com/'
+HOT_MANGA_URL = 'https://mangakakalot.com/manga_list?type=topview&category=all&state=all&page=1'
 
 def scrape_latest_update_manga():
     anime_list = []
@@ -30,5 +31,33 @@ def scrape_latest_update_manga():
     except Exception as e:
         print(e)
         return {'error': str(e)}
+
+    
+    def scrape_hot_manga():
+    anime_list = []
+    try:
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
+        response = requests.get(HOT_MANGA_URL, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        for el in soup.select('div.truyen-list > .list-truyen-item-wrap'):
+            anime_list.append({
+                'mangaId': el.select_one('a.list-story-item.bookmark_check')['href'],
+                'mangaTitle': el.select_one('a.list-story-item.bookmark_check')['title'],
+                'mangaImg': el.select_one('a.list-story-item.bookmark_check > img')['src'],
+                'chapter': el.select_one('a.list-story-item-wrap-chapter').text.strip(),
+                'mangaUrl': el.select_one('a.list-story-item.bookmark_check')['href'],
+                'views': el.select_one('span.aye_icon').text.strip()
+            })
+
+        with open('./mangakakalot/latest-update.json', 'w') as f:
+            json.dump(anime_list, f, indent=2)
+        print('Data saved to mangakakalot/latest-update.json')
+
+        return anime_list
+    except Exception as e:
+        print(e)
+        return {'error': str(e)}
     
 scrape_latest_update_manga()
+scrape_hot_manga()
