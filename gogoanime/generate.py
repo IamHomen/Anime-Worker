f45656b79fcd0ea5f04af6c112ae000bbdf022b3
Homen <50195844+IamHomen@users.noreply.github.com>
2024-04-10 19:57:13 +0800

    Update generate.py

--- gogoanime/generate.py
+++ gogoanime/generate.py
@@ -14,6 +14,7 @@ def scrape_recent_sub_anime():
         soup = BeautifulSoup(response.content, 'html.parser')
 
         for el in soup.select('div.last_episodes.loaddub > ul > li'):
+            ids = el.select_one('p.name > a')['href'].split('/')[1].split('-episode-')[0]
             anime_list.append({
                 'animeId': el.select_one('p.name > a')['href'].split('/')[1].split('-episode-')[0],
                 'animeTitle': el.select_one('p.name > a')['title'],
@@ -21,6 +22,7 @@ def scrape_recent_sub_anime():
                 'episode': el.select_one('p.episode').text.strip(),
                 'animeUrl': BASE_URL + el.select_one('p.name > a')['href']
             })
+            scrape_anime_info(ids)
 
         with open('./gogoanime/recent-sub.json', 'w') as f:
             json.dump(anime_list, f, indent=2)
@@ -30,6 +32,69 @@ def scrape_recent_sub_anime():
     except Exception as e:
         print(e)
         return {'error': str(e)}
+
+def scrape_anime_info(ids):
+    try:
+        genres = []
+        epList = []
+
+        animePageTest = requests.get(f'https://gogoanime.gg/category/{ids}')
+        animePageTest.raise_for_status()
+
+        soup = BeautifulSoup(animePageTest.content, 'html.parser')
+
+        animeTitle = soup.select_one('div.anime_info_body_bg > h1').text.strip()
+        animeImage = soup.select_one('div.anime_info_body_bg > img')['src']
+        type = soup.select_one('div.anime_info_body_bg > p:nth-child(4) > a').text.strip()
+        desc = soup.select_one('div.anime_info_body_bg > p:nth-child(5)').text.replace('Plot Summary: ', '').strip()
+        releasedDate = soup.select_one('div.anime_info_body_bg > p:nth-child(7)').text.replace('Released: ', '').strip()
+        status = soup.select_one('div.anime_info_body_bg > p:nth-child(8) > a').text.strip()
+        otherName = soup.select_one('div.anime_info_body_bg > p:nth-child(9)').text.replace('Other name: ', '').replace(';', ',').strip()
+
+        for genre in soup.select('div.anime_info_body_bg > p:nth-child(6) > a'):
+            genres.append(genre['title'].strip())
+
+        ep_start = soup.select_one('#episode_page > li').find('a')['ep_start']
+        ep_end = soup.select_one('#episode_page > li:last-child').find('a')['ep_end']
+        movie_id = soup.select_one('#movie_id')['value']
+        alias = soup.select_one('#alias_anime')['value']
+
+        list_episodes_url = 'https://example.com/list_episodes'  # Replace with the actual URL
+        html = requests.get(f'{list_episodes_url}?ep_start={ep_start}&ep_end={ep_end}&id={movie_id}&default_ep=0&alias={alias}')
+        html.raise_for_status()
+
+        $$ = BeautifulSoup(html.content, 'html.parser')
+
+        for el in $$('#episode_related > li'):
+            episodeLocale = el.select_one('div.cate').text.strip().lower()
+            epList.append({
+                'episodeId': el.find('a')['href'].split('/')[1],
+                'episodeNum': el.select_one('div.name').text.replace('EP ', '').strip(),
+                'episodeUrl': BASE_URL + el.find('a')['href'].strip(),
+                'isSubbed': episodeLocale == "sub",
+                'isDubbed': episodeLocale == "dub"
+            })
+
+        anime_data = {
+            'animeTitle': str(animeTitle),
+            'type': str(type),
+            'releasedDate': str(releasedDate),
+            'status': str(status),
+            'genres': genres,
+            'otherNames': otherName,
+            'synopsis': str(desc),
+            'animeImg': str(animeImage),
+            'totalEpisodes': ep_end,
+            'episodesList': epList,
+        }
+
+        with open(f'./gogoanime/anime-info/{id}.json', 'w') as json_file:
+            json.dump(anime_data, json_file, indent=2)
+
+        return anime_data
+    except Exception as err:
+        print(err)
+        return {'error': str(err)}
 
 POPULAR_URL = 'https://anitaku.to/popular.html'
 
