2726878a1208d96b6cb5a567c0a977f2f9133189
Homen <50195844+IamHomen@users.noreply.github.com>
2024-04-11 13:28:43 +0800

    Update generate.py

--- gogoanime/generate.py
+++ gogoanime/generate.py
@@ -33,12 +33,72 @@ def scrape_recent_sub_anime():
         print(e)
         return {'error': str(e)}
 
+POPULAR_URL = 'https://anitaku.to/popular.html'
+
+def scrape_popular_anime():
+    anime_list = []
+    try:
+        headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36'}
+        popular_page = requests.get(POPULAR_URL, headers=headers)
+        soup = BeautifulSoup(popular_page.content, 'html.parser')
+
+        for el in soup.select('div.last_episodes > ul > li'):
+            ids = el.select_one('p.name > a')['href'].split('/')[2]
+            anime_list.append({
+                'animeId': el.select_one('p.name > a')['href'].split('/')[2],
+                'animeTitle': el.select_one('p.name > a')['title'],
+                'animeImg': el.select_one('div > a > img')['src'],
+                'releasedDate': el.select_one('p.released').text.replace('Released: ', '').strip(),
+                'animeUrl': BASE_URL + el.select_one('p.name > a')['href']
+            })
+            scrape_anime_info(ids)
+
+        with open('./gogoanime/popular.json', 'w') as f:
+            json.dump(anime_list, f, indent=2)
+        print('Data saved to gogoanime/popular.json')
+
+        return anime_list
+    except Exception as e:
+        print(e)
+        return {'error': str(e)}
+
+popular_ongoing_url = 'https://ajax.gogocdn.net/ajax/page-recent-release-ongoing.html?page=1'
+
+def scrape_trending_anime():
+    anime_list = []
+    try:
+        headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36'}
+        pageNum = 1
+        popular_page = requests.get(popular_ongoing_url, headers=headers)
+        soup = BeautifulSoup(popular_page.content, 'html.parser')
+
+        for el in soup.select('div.added_series_body.popular > ul > li'):
+            ids = el.select_one('a:nth-child(1)')['href'].split('/')[2],
+            genres = [a['title'] for a in el.select('p.genres > a')]
+            anime_list.append({
+                'animeId': el.select_one('a:nth-child(1)')['href'].split('/')[2],
+                'animeTitle': el.select_one('a:nth-child(1)')['title'],
+                'animeImg': el.select_one('a:nth-child(1) > div')['style'].split('(')[1].split(')')[0],
+                'latestEp': el.select_one('p:nth-child(4) > a').text.strip(),
+                'animeUrl': BASE_URL + el.select_one('a:nth-child(1)')['href'],
+                'genres': genres
+            })
+            scrape_anime_info(ids)
+
+        with open('./gogoanime/trending.json', 'w') as f:
+            json.dump(anime_list, f, indent=2)
+        print('Data saved to gogoanime/trending.json')
+        return anime_list
+    except Exception as e:
+        print(e)
+        return {'error': str(e)}
+
 def scrape_anime_info(ids):
     try:
         genres = []
         epList = []
 
-        animePageTest = requests.get(f'https://gogoanime.gg/category/{ids}')
+        animePageTest = requests.get(f'https://anitaku.so/category/{ids}')
         animePageTest.raise_for_status()
 
         soup = BeautifulSoup(animePageTest.content, 'html.parser')
@@ -91,62 +151,6 @@ def scrape_anime_info(ids):
     except Exception as err:
         print(err)
         return {'error': str(err)}
-
-POPULAR_URL = 'https://anitaku.to/popular.html'
-
-def scrape_popular_anime():
-    anime_list = []
-    try:
-        headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36'}
-        popular_page = requests.get(POPULAR_URL, headers=headers)
-        soup = BeautifulSoup(popular_page.content, 'html.parser')
-
-        for el in soup.select('div.last_episodes > ul > li'):
-            anime_list.append({
-                'animeId': el.select_one('p.name > a')['href'].split('/')[2],
-                'animeTitle': el.select_one('p.name > a')['title'],
-                'animeImg': el.select_one('div > a > img')['src'],
-                'releasedDate': el.select_one('p.released').text.replace('Released: ', '').strip(),
-                'animeUrl': BASE_URL + el.select_one('p.name > a')['href']
-            })
-
-        with open('./gogoanime/popular.json', 'w') as f:
-            json.dump(anime_list, f, indent=2)
-        print('Data saved to gogoanime/popular.json')
-
-        return anime_list
-    except Exception as e:
-        print(e)
-        return {'error': str(e)}
-
-popular_ongoing_url = 'https://ajax.gogocdn.net/ajax/page-recent-release-ongoing.html?page=1'
-
-def scrape_trending_anime():
-    anime_list = []
-    try:
-        headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36'}
-        pageNum = 1
-        popular_page = requests.get(popular_ongoing_url, headers=headers)
-        soup = BeautifulSoup(popular_page.content, 'html.parser')
-
-        for el in soup.select('div.added_series_body.popular > ul > li'):
-            genres = [a['title'] for a in el.select('p.genres > a')]
-            anime_list.append({
-                'animeId': el.select_one('a:nth-child(1)')['href'].split('/')[2],
-                'animeTitle': el.select_one('a:nth-child(1)')['title'],
-                'animeImg': el.select_one('a:nth-child(1) > div')['style'].split('(')[1].split(')')[0],
-                'latestEp': el.select_one('p:nth-child(4) > a').text.strip(),
-                'animeUrl': BASE_URL + el.select_one('a:nth-child(1)')['href'],
-                'genres': genres
-            })
-
-        with open('./gogoanime/trending.json', 'w') as f:
-            json.dump(anime_list, f, indent=2)
-        print('Data saved to gogoanime/trending.json')
-        return anime_list
-    except Exception as e:
-        print(e)
-        return {'error': str(e)}
 
 scrape_recent_sub_anime()
 scrape_trending_anime()
