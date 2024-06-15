04726939a107dba6807342ef02074bb2027b4db4
Homen <50195844+IamHomen@users.noreply.github.com>
2024-06-15 12:39:23 +0800

    Update generate.py

--- gogoanime/generate.py
+++ gogoanime/generate.py
@@ -15,6 +15,15 @@ NEW_SEASON_URL = 'https://anitaku.so/new-season.html'
 MOVIE_URL = 'https://anitaku.so/anime-movies.html'
 load_dotenv()
 
+# Load Firebase service account key from environment variable
+service_account_info = json.loads(os.getenv('FIREBASE_SERVICE_ACCOUNT'))
+
+# Initialize the Firebase app
+cred = credentials.Certificate(service_account_info)
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://mrcain-12665-default-rtdb.firebaseio.com'
+})
+
 def scrape_recent_sub_anime():
     anime_list = []
     try:
@@ -272,24 +281,19 @@ def scrape_anime_info(ids):
             'totalEpisodes': ep_end,
             #'episodesList': epList,
         }
-
-        # Load Firebase service account key from environment variable
-        service_account_info = json.loads(os.getenv('FIREBASE_SERVICE_ACCOUNT'))
-
-        # Initialize the Firebase app
-        cred = credentials.Certificate(service_account_info)
-        firebase_admin.initialize_app(cred, {
-            'databaseURL': 'https://mrcain-12665-default-rtdb.firebaseio.com'
-        })
-
-        # Specify the reference where you want to upload the data
-        ref = db.reference(f'/anime-list/anime/info/{ids}')
-
-        # Upload the data
-        ref.set(anime_data)
         
-        # with open(f'./gogoanime/anime-info/{ids}.json', 'w') as json_file:
-        #    json.dump(anime_data, json_file, indent=2)
+         with open(f'./gogoanime/anime-info/{ids}.json', 'w') as json_file:
+            json.dump(anime_data, json_file, indent=2)
+             
+         # Load the JSON data from a file
+         with open(f'./gogoanime/anime-info/{ids}.json', 'r') as f:
+             data = json.load(f)
+         # Specify the reference where you want to upload the data
+         ref = db.reference(f'/anime-list/anime/info/{ids}')
+         # Upload the data
+         ref.set(data)
+        
+         os.remove(f'./gogoanime/anime-info/{ids}.json')
 
         return anime_data
     except Exception as err:
