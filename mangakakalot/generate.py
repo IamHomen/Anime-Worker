64af84d03d3da3d02fadfa44a485e6603731a4fb
Homen <masterzense16@gmail.com>
2025-02-17 11:55:19 +0800

    Update generate.py

--- mangakakalot/generate.py
+++ mangakakalot/generate.py
@@ -5,6 +5,7 @@ import json
 LATEST_MANGA_URL = 'https://manganato.com/genre-all/'
 BASE_URL = 'https://manganato.com/'
 HOT_MANGA_URL = 'https://mangakakalot.com/manga_list?type=topview&category=all&state=all&page=1'
+NEWEST_MANGA_URL = 'https://m.manganelo.com/genre-all/'
 
 '''def scrape_latest_update_manga():
     anime_list = []
@@ -168,7 +169,68 @@ def scrape_most_viewed_manga():
     except Exception as e:
         print(f"An error occurred: {e}")
         return {'error': str(e)}
+
+def scrape_newest_manga():
+    anime_list = []
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+        }
+        page_number = 1
+
+        while page_number <= 1968:
+            print(f"Scraping page {page_number}...")
+            url = f"{NEWEST_MANGA_URL}{page_number}?type=newest"
+            response = requests.get(url, headers=headers)
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            manga_items = soup.select('div.content-genres-item')
+            if not manga_items:
+                break  # Stop if no more pages
+
+            for el in manga_items:
+                title = el.select_one('a.genres-item-img.bookmark_check')['title']
+                img = el.select_one('a.genres-item-img.bookmark_check > img')['src']
+                
+                chapter_element = el.select_one('a.genres-item-chap')
+                chapter = chapter_element.text.strip() if chapter_element else "No chapter"
+                chapter_url = chapter_element.get('href', 'No URL').split("/")[-1] if chapter_element else "No URL"
+
+                manga_element = el.select_one('a.genres-item-img.bookmark_check')
+                manga_url = manga_element.get('href', 'No URL').split("/")[-1] if manga_element else "No URL"
+
+                description_element = el.select_one('.genres-item-description')
+                description = description_element.text.strip() if description_element else "N/A"
+
+                views_element = el.select_one('.genres-item-view')
+                views = views_element.text.strip() if views_element else "0"
+                
+                time_element = el.select_one('.genres-item-time')
+                time = time_element.text.strip() if time_element else "Unknown"
+
+                anime_list.append({
+                    'mangaTitle': title,
+                    'mangaImg': img,
+                    'chapter': chapter,
+                    'chapterUrl': chapter_url,
+                    'mangaUrl': manga_url,
+                    'description': description,
+                    'views': views,
+                    'time': time
+                })
+
+            page_number += 1
+
+        with open('./mangakakalot/newest-manga.json', 'w') as f:
+            json.dump(anime_list, f, indent=2)
+        print('Data saved to mangakakalot/newest.json')
+
+        return anime_list
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return {'error': str(e)}
     
 scrape_latest_update_manga()
 scrape_hot_manga()
 scrape_most_viewed_manga()
+scrape_newest_manga()
